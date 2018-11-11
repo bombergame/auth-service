5df030ec0d1b1c551e5e26b120ba4fd60a29e3d9
denis96z <zinoviev.denis@yandex.ru>
2018-11-11 21:13:16 +0300

    profiles-service grpc client implementation

--- config/config.go
+++ config/config.go
@@ -2,9 +2,15 @@ package config
 
 import (
 	"github.com/bombergame/common/args"
+	"github.com/bombergame/common/env"
 )
 
 var (
 	HttpPort = args.GetString("http_port", "80")
 	GrpcPort = args.GetString("grpc_port", "3000")
+
+	ProfilesServiceGrpcAddress = env.GetVar(
+		"PROFILES_SERVICE_GRPC_ADDRESS",
+		"profiles-service:3000",
+	)
 )
